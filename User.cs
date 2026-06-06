df308c8d410e9d00276fdce48d2ff8c6fea038f2
MF247293 <247293@Students.au.edu.pk>
2026-06-06 21:05:19 +0500

    creatr user.cs file

---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    [Required(ErrorMessage = "First name required")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name required")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Email required")]
+    [EmailAddress(ErrorMessage = "Invalid email")]
+    public string Email { get; set; }
+}
